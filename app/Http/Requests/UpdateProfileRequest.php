0f57ebf32e634cae12630ef1e2c446c883e6cda7
Fahed <you@example.com>
2026-08-16 13:14:10 +0300

    add profile image and edit profile for user

---
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProfileRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name'                => 'sometimes|string|max:255',
+            'bio'                 => 'nullable|string|max:1000',
+            'languages'           => 'nullable|array',
+            'languages.*'         => 'string',
+            'is_matching_enabled' => 'sometimes|boolean',
+        ];
+    }
+}
